aba1a41c19385bddf000e5360549c2c154633f5b
jvtang487 <jvtang@ucsd.edu>
2023-01-26 12:58:53 -0800

    fix bugs in reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,12 +7,26 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
 	}
+
+  @Test
+  public void testReverseMultipleNumbers() {
+    int[]input = {1, 2, 3, 4};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{4,3,2,1}, input);
+  }
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2(){
+    int[] input1 = {3, 4, 5, 6 };
+    assertArrayEquals(new int[]{6, 5, 4, 3}, ArrayExamples.reversed(input1));
   }
 }
